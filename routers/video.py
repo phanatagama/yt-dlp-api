0f29278d8f1ee15589298a4a8b304c03935d7eef
phanatagama <48324618+phanatagama@users.noreply.github.com>
2025-01-05 05:33:16 +0700

    feat: move cookies to tmp

--- routers/video.py
+++ routers/video.py
@@ -9,10 +9,17 @@ async def get_api_version():
 
 async def extract_video_info(video_url: str = ''):
     # po_token ="MnQ4RCUX1rkNwTh8YuYQC-fXgf_g3KsJY3NyPsBPBUBzQRBT6q0ZHOE7QPT4k8WRvAXqpRUps_NkCGVvZN6OuwYL0ItXqkMi4iqfWjvDrKduMM2kckSI7nwU1W2ElNr_1aqIQ1M3gLWQqxM9IunkAos9X4dCSA=="
-    cookies_file = "cookies.txt"  # Path to your cookies file
+    # cookies_file = "cookies.txt"  # Path to your cookies file
+    # copy cookies.txt into /tmp
+    path = "/tmp"
+
+    # Join various path components
+    tmp_cookies = os.path.join(path, "cookies.txt")
+
+    dest = shutil.copyfile("cookies.txt", tmp_cookies)
 
     ydl_opts = {
-        'cookiefile': cookies_file,
+        'cookiefile': tmp_cookies,
         # 'po_token':f"web+{po_token}",
         'quiet': True,
         'simulate': True,
