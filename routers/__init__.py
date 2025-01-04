bd8d4000dd3b6ec42ccc78641de173785079733c
phanatagama <48324618+phanatagama@users.noreply.github.com>
2025-01-05 05:40:45 +0700

    feat: update ping endpoint

--- routers/__init__.py
+++ routers/__init__.py
@@ -6,7 +6,7 @@ info_router = APIRouter(tags=['API version'])
 info_router.add_api_route('/', get_api_version, methods=['GET'])
 
 ping_router = APIRouter(tags=['API ping'])
-ping_router.add_api_route('/', ping, methods=['GET'])
+ping_router.add_api_route('/ping', ping, methods=['GET'])
 
 video_info_router = APIRouter(tags=['Extract video_info'])
 video_info_router.add_api_route('/video/', extract_video_info, methods=['GET'])
